3d9e7f989486ad145c97e81362e821b2eee42763
Sergey Beryozkin <sberyozkin@gmail.com>
2018-11-06 13:45:27 +0000

    [THORN-2225] Adding one more test

--- testsuite/testsuite-jaxrs-jsonb/src/test/java/org/wildfly/swarm/jaxrs/JaxrsJsonbTest.java
+++ testsuite/testsuite-jaxrs-jsonb/src/test/java/org/wildfly/swarm/jaxrs/JaxrsJsonbTest.java
@@ -10,6 +10,7 @@ import javax.ws.rs.core.MediaType;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.resteasy.plugins.providers.jsonb.JsonBindingProvider;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.junit.After;
@@ -54,10 +55,29 @@ public class JaxrsJsonbTest {
         Jsonb jsonb = JsonbBuilder.create();
         
         String response = client.target("http://localhost:8080/jsonb")
-                                .request(MediaType.APPLICATION_JSON)
-                                .post(Entity.entity(jsonb.toJson(dog), MediaType.APPLICATION_JSON),
-                                String.class);
+                              .request(MediaType.APPLICATION_JSON)
+                              .post(Entity.entity(jsonb.toJson(dog), MediaType.APPLICATION_JSON),
+                              String.class);
         Dog dog2 = jsonb.fromJson(response, Dog.class);
+        Assert.assertEquals(dog.name, dog2.name);
+        Assert.assertEquals(dog.age, dog2.age);
+        Assert.assertTrue(dog2.bitable);
+    }
+
+    @Test
+    @RunAsClient
+    public void testJaxrsJsonbWithProvider() throws Exception {
+        Dog dog = new Dog();
+        dog.name = "Falco";
+        dog.age = 4;
+        dog.bitable = false;
+
+        Dog dog2 = client.target("http://localhost:8080/jsonb")
+                       .register(new JsonBindingProvider())
+                       .request(MediaType.APPLICATION_JSON)
+                       .post(Entity.entity(dog, MediaType.APPLICATION_JSON),
+                       Dog.class);
+        
         Assert.assertEquals(dog.name, dog2.name);
         Assert.assertEquals(dog.age, dog2.age);
         Assert.assertTrue(dog2.bitable);
