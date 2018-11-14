44bc718c780bb5c345b9880ede5a955f8739a2b4
Sergey Beryozkin <sberyozkin@gmail.com>
2018-11-14 15:21:18 +0000

    Getting the servlet test passing

--- testsuite/testsuite-ee-security/src/test/java/org/wildfly/swarm/ee/security/EESecurityTest.java
+++ testsuite/testsuite-ee-security/src/test/java/org/wildfly/swarm/ee/security/EESecurityTest.java
@@ -20,25 +20,37 @@ import static org.fest.assertions.Assertions.assertThat;
 import java.io.IOException;
 
 import org.apache.http.client.fluent.Request;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wildfly.swarm.arquillian.DefaultDeployment;
+import org.wildfly.swarm.undertow.WARArchive;
 
 @RunWith(Arquillian.class)
-@DefaultDeployment
 public class EESecurityTest {
 
 
+    @Deployment
+    public static Archive createDeployment() throws Exception {
+        WARArchive deployment = ShrinkWrap.create(WARArchive.class);
+        deployment.addClass(EESecurityServlet.class);
+        deployment.addClass(SimpleAuthenticationMechanism.class);
+        deployment.addClass(SimpleIdentityStore.class);
+        deployment.addAsWebInfResource("jboss-web.xml");
+        return deployment;
+    }
+    
     @Test
     @RunAsClient
     public void testCustomAuthenticationMechanism() throws IOException {
         String response1 = Request.Get("http://localhost:8080/security?name=thorntail1&password=secret1")
                 .execute().returnContent().asString().trim();
-        assertThat(response1).isEqualTo("thorntail1,role1:true,role2:false");
-        String response2 = Request.Get("http://localhost:8080/security?name=thorntail1&password=secret2")
+        assertThat(response1).isEqualTo("thorntail1, role1:true, role2:false");
+        String response2 = Request.Get("http://localhost:8080/security?name=thorntail2&password=secret2")
                 .execute().returnContent().asString().trim();
-        assertThat(response2).isEqualTo("thorntail1,role1:true,role2:false");
+        assertThat(response2).isEqualTo("thorntail2, role1:false, role2:true");
     }
 }
